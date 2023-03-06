6dac1e13fa09042a0efa54235d12d1c882cc35d6
sumit.jaiswal <sumit.jaiswal@salesforce.com>
2023-03-06 11:50:59 +0530

    added code to test

---
+++ create_server.tf
@@ -0,0 +1,16 @@
+# Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a new EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
+  security_groups = ["default"]
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
